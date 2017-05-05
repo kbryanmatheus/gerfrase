14385df96e676f817974551d50986f71a24bf6d2
kbryanmatheus <kbryanmatheus@yahoo.com.br>
2017-05-05 09:26:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require 'logica.php'; ?>
+
+
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> <?= "Olá $tratamentos $nome, $qualidades $lugares";  ?> </h1>
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,9 @@
+<?php
+    $tratamento = ['Dom','Lord','Sir','Magnifico','imponente','Colossal'];
+    $qualidade  = ['Paladino','Cavaleiro','Nandertal','Desbravador','Pistoleiro'];
+    $lugar      = ['Do vale do silêncio','Do mercadão de peixes','Do beco','Da terra da minhoca','De Onde Ninguem vem'];
+    $nome       = "Malaquias";
+
+    $tratamentos = $tratamento[rand(0,5)];
+    $qualidades  = $qualidade[rand(0,4)];
+    $lugares     = $lugar[rand(0,4)];
